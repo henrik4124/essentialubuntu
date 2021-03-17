324848a839cc46f13ab2bf7ddfa6b32cfdf26ea8
henrik4124 <44812275+henrik4124@users.noreply.github.com>
2021-03-17 17:26:52 +0100

    Add files via upload

--- eu.sh
+++ eu.sh
@@ -4,17 +4,31 @@ green=`tput setaf 2`
 nc=`tput sgr0`
 
 echo "${green}"
-echo "   _____  ____ ________     _____ /\       ___________ ____ ___ ";
-echo "  /  |  |/_   |\_____  \   /  |  |)/______ \_   _____/|    |   \";
-echo " /   |  |_|   | /  ____/  /   |  |_/  ___/  |    __)_ |    |   /";
-echo "/    ^   /|   |/       \ /    ^   /\___ \   |        \|    |  / ";
-echo "\____   | |___|\_______ \\____   |/____  > /_______  /|______/  ";
-echo "     |__|              \/     |__|     \/          \/           ";
+echo "   _____  ____ ________     _____ /\       ___________ ____ ___  ";
+echo "  /  |  |/_   |\_____  \   /  |  |)/______ \_   _____/|    |   \ ";
+echo " /   |  |_|   | /  ____/  /   |  |_/  ___/  |    __)_ |    |   / ";
+echo "/    ^   /|   |/       \ /    ^   /\___ \   |        \|    |  /  ";
+echo "\____   | |___|\_______ \\____   |/____  > /_______  /|______/   ";
+echo "     |__|              \/     |__|     \/          \/            ";
 echo " ${nc}"
 echo " The script will install all the essentials for ubuntu in 5 sec. "
 
 sudo apt update
 
+sleep 2
+
+## sublime
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+sleep 2
+sudo apt-get install apt-transport-https
+sleep 2
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+sleep 2
+sudo apt-get update
+sleep 2
+sudo apt-get install sublime-text
+clear
+echo "sublime installed!"
 sleep 2
 
 ## discord
@@ -40,21 +54,21 @@ sleep 3
 clear
 
 ## wine & lutris (for spilling)
-dpkg --add-architecture i386 
-sleep 1
+sudo dpkg --add-architecture i386 
+sleep 2
 wget -nc https://dl.winehq.org/wine-builds/winehq.key
-sleep 1
+sleep 2
 sudo apt-key add winehq.key
-sleep 1
+sleep 2
 sudo apt-add-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ focal main'
-sleep 1
+sleep 2
 sudo apt update
 sudo apt install --install-recommends winehq-stable
 echo "Wine installed!"
 sleep 3
 clear
 sudo add-apt-repository ppa:lutris-team/lutris
-sleep 1
+sleep 2
 sudo apt update
 sudo apt install lutris
 echo "Lutris installed!"
